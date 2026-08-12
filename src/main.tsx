8927e2a8739593c0338f9a31cec4c1c3f56cc516
lucasvenancio0110 <lucassantanals0110@gmail.com>
2026-08-12 11:56:51 -0300

    load block editor and conflict styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './app/App';
 import './styles/app.css';
 import './styles/report-editor.css';
+import './styles/block-editor-conflicts.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
