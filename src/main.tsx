4fa7aaaa5d44e140a51dc0042c76fe0cc5b55d3a
lucasvenancio0110 <lucassantanals0110@gmail.com>
2026-08-14 04:35:04 -0300

    activate industrial HMI stylesheet

--- src/main.tsx
+++ src/main.tsx
@@ -11,6 +11,7 @@ import './styles/clean-cockpit.css';
 import './styles/assisted-validation.css';
 import './styles/v4-cockpit.css';
 import './styles/v5-workspace.css';
+import './styles/factory-floor.css';
 
 const preventGestureZoom = (event: Event) => event.preventDefault();
 document.addEventListener('gesturestart', preventGestureZoom, { passive: false });
